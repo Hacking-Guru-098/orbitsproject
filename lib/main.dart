48a7fe7cc896f3227cbda7af40f5e70368ff0707
Arman Maurya <armanmarya6@gmail.com>
2025-03-31 23:59:20 +0530

    Created the Dashboard Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/login.dart';
+import 'package:orbitsproject/pages/dashboard.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(),
+      home: DashboardPage(),
     );
   }
 }
